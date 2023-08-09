9094412f7c62b9d89b61dba9a38c7ce8f37af0b4
alexissdev <notcacha1998@gmail.com>
2023-08-08 21:31:42 -0300

    deps: added the commons dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,10 @@ plugins {
 
 subprojects {
     apply(plugin = "alexissdev.spigot-conventions")
+
+    dependencies {
+        implementation("dev.alexisdev:commons-bukkit:0.0.1")
+        implementation("dev.alexisdev:commons-error:0.0.1")
+        implementation("dev.alexisdev:commons-validate:0.0.1")
+    }
 }
